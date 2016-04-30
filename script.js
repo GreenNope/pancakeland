66b7d09f36bdaa1f1a9a81fd67fc89c415dae3b8
GreenNope <tlash1000@gmail.com>
2016-04-29 21:45:55 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function textAppear(textID) {
-  document.getElementById('upgradestext').style.display = "none";
+  document.getElementById('farmtext').style.display = "none";
   document.getElementById('farmtext').style.display = "none";
   // no disables come after here please :)
   document.getElementById(textID).style.display = "inline.block";
